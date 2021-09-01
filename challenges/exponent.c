0ddf14044933cb2aec6097e3e1d812218a611e36
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-08-31 22:38:13 -0300

    Exponent challenge - DONE

---
+++ challenges/exponent.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void exponent(int a, int b)
+{
+    int result = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    printf("%dˆ%d: %d\n", a, b, result);
+}
+
+int main()
+{
+    exponent(2, 5);
+}
