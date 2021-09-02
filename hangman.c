60c900e8f0b687acd6897f78b0a48cd2fa91f966
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-09-02 20:22:03 -0300

    New function - find_letter

--- hangman.c
+++ hangman.c
@@ -17,6 +17,18 @@ void requests_user_attempt(char storaged_attempts[26], int* attempts_counter)
     (*attempts_counter)++;
 }
 
+int find_letter(char letter, char storaged_attempts[26], int attempts_counter)
+{
+    int found_letter = 0;
+    for(int y = 0; y < attempts_counter; y++){
+        if(storaged_attempts[y] == letter){
+            found_letter = 1;
+            break; 
+        }
+    }
+    return found_letter;
+}
+
 int main(void)
 {
     char secret_word[20];
@@ -31,14 +43,7 @@ int main(void)
     do
     {
         for(int x = 0; x < strlen(secret_word); x++){
-            int found_letter = 0;
-            for(int y = 0; y < attempts_counter; y++){
-                if(storaged_attempts[y] == secret_word[x]){
-                    found_letter = 1;
-                    break; 
-                }
-            }
-
+            int found_letter = find_letter(secret_word[x],  storaged_attempts, attempts_counter);
             if(found_letter){
                 printf("%c ", secret_word[x]);
             }else{
