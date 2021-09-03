9a2bf81089b0747b004e89b5814fc2d101ddba39
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-09-02 21:06:59 -0300

    New functions - print_word() and pick_word()

--- hangman.c
+++ hangman.c
@@ -29,6 +29,24 @@ int find_letter(char letter, char storaged_attempts[26], int attempts_counter)
     return found_letter;
 }
 
+void print_word(char secret_word[20], char storaged_attempts[26], int attempts_counter)
+{
+    for(int x = 0; x < strlen(secret_word); x++){
+        int found_letter = find_letter(secret_word[x],  storaged_attempts, attempts_counter);
+        if(found_letter){
+            printf("%c ", secret_word[x]);
+        }else{
+            printf("- ");
+        }
+    }
+    printf("\n"); 
+}
+
+void pick_word(char secret_word[20])
+{
+    sprintf(secret_word, "MELANCIA ");//It fills the secret_word variable with the text given
+}
+
 int main(void)
 {
     char secret_word[20];
@@ -37,21 +55,12 @@ int main(void)
     int player_won = 0;
     int hanged = 0;
 
-    sprintf(secret_word, "MELANCIA ");//It fills the secret_word variable with the text given
+    pick_word(secret_word);
     write_header();
     
     do
     {
-        for(int x = 0; x < strlen(secret_word); x++){
-            int found_letter = find_letter(secret_word[x],  storaged_attempts, attempts_counter);
-            if(found_letter){
-                printf("%c ", secret_word[x]);
-            }else{
-                printf("- ");
-            }
-        }
-        printf("\n"); 
-
+        print_word(secret_word, storaged_attempts, attempts_counter);
         requests_user_attempt(storaged_attempts, &attempts_counter);
     }while(!player_won && !hanged);
 }
